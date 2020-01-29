19c8d344b028379a75e3583a34c1a6d718c2b8e3
Ivan Rudiuk <IRudiuk@its.jnj.com>
2020-01-29 15:20:29 -0500

    Add building docs app as part of edit env build for p.sh

--- packages/bodiless-psh/resources/edit/platform.custom.sh
+++ packages/bodiless-psh/resources/edit/platform.custom.sh
@@ -22,7 +22,8 @@ if [ "$1" = "install" ]; then
   npm ci
 elif [ "$1" = "build" ]; then
   npm run build
-  npm run build:env-vars
+  npx lerna run build:env-vars --scope @bodiless/test-site
+  npx lerna run build:doc --scope @bodiless/test-site
 else
   echo "Unknown command specified to $0"
 fi
